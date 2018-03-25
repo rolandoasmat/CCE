a1d9b41fc4ba64a95f08bebed67fdc4c389c86ec
Rolando Asmat <rolandoasmat@gmail.com>
2018-03-25 09:07:43 -0500

    Added tree example

--- Chapter 4. Trees and Graphs/src/com/rolandoasmat/Main.java
+++ Chapter 4. Trees and Graphs/src/com/rolandoasmat/Main.java
@@ -3,6 +3,10 @@ package com.rolandoasmat;
 public class Main {
 
     public static void main(String[] args) {
+        runTree();
+    }
+
+    public static void runGraph() {
         // Nodes
         Node a = new Node("A");
         Node b = new Node("B");
@@ -30,5 +34,32 @@ public class Main {
 
         System.out.println("Depth First Search");
         graph.depthFirstSearch();
+    }
+
+    public static void runTree() {
+        Tree.Node one = new Tree.Node(1);
+        Tree.Node two = new Tree.Node(2);
+        Tree.Node three = new Tree.Node(3);
+        Tree.Node four = new Tree.Node(4);
+        Tree.Node five = new Tree.Node(5);
+        Tree.Node six = new Tree.Node(6);
+        Tree.Node seven = new Tree.Node(7);
+        Tree.Node eight = new Tree.Node(8);
+        Tree.Node nine = new Tree.Node(9);
+
+        one.left = two;
+        one.right = three;
+        two.left = four;
+        two.right = five;
+        three.left = six;
+        three.right = seven;
+        four.left = eight;
+        four.right = nine;
+
+        Tree tree = new Tree(one);
+        Tree.Node nodeToFind = one;
+        int level = tree.findLevelOfNode(nodeToFind);
+        System.out.println("Level of Node: "+ nodeToFind.data+" is " +level);
+
     }
 }
