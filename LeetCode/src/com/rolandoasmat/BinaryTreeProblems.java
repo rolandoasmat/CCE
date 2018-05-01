96735a9aa050eeb798af1eb3375f033da78db85f
Rolando Asmat <rolandoasmat@gmail.com>
2018-05-01 17:31:02 -0500

    Count uni val subtrees

--- LeetCode/src/com/rolandoasmat/BinaryTreeProblems.java
+++ LeetCode/src/com/rolandoasmat/BinaryTreeProblems.java
@@ -77,6 +77,52 @@ public class BinaryTreeProblems {
         }
     }
 
+    /**
+     * Count Univalue Subtrees
+     * Given a binary tree, count the number of uni-value subtrees.
+     * A Uni-value subtree means all nodes of the subtree have the same value.
+     *
+     * https://leetcode.com/problems/count-univalue-subtrees/description/
+     */
+    public int countUnivalSubtrees(TreeNode root) {
+        Count count = new Count();
+        isUni(root, count);
+        return count.count;
+    }
+    class Count {
+        int count = 0;
+    }
+    public boolean isUni(TreeNode root, Count count) {
+        if (root == null) {
+            return true;
+        } else if (root.left == null && root.right == null) {
+            count.count += 1;
+            return true;
+        } else {
+            boolean isLeftSubtreeUni = isUni(root.left, count);
+            boolean isRightSubtreeUni = isUni(root.right, count);
+            if (isLeftSubtreeUni && isRightSubtreeUni) {
+                if (root.left != null && root.right != null) {
+                    if (root.left.val == root.val && root.right.val == root.val) {
+                        count.count += 1;
+                        return true;
+                    }
+                } else if (root.left != null && root.right == null) {
+                    if (root.left.val == root.val) {
+                        count.count += 1;
+                        return true;
+                    }
+                } else if (root.right != null && root.left == null) {
+                    if (root.right.val == root.val) {
+                        count.count += 1;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
 }
 
 
