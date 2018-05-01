43a6ab1cdae59f52f51f57dd0187e0303f28a455
Rolando Asmat <rolandoasmat@gmail.com>
2018-05-01 16:21:46 -0500

    has path sum

--- LeetCode/src/com/rolandoasmat/BinaryTreeProblems.java
+++ LeetCode/src/com/rolandoasmat/BinaryTreeProblems.java
@@ -58,6 +58,25 @@ public class BinaryTreeProblems {
             return false;
         }
     }
+
+    /**
+     * Path Sum
+     * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such
+     * that adding up all the values along the path equals the given sum.
+     *
+     * https://leetcode.com/problems/path-sum/description/
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return  false;
+        } else if (root.left == null && root.right == null) {
+            return sum == root.val;
+        } else {
+            int val = sum - root.val;
+            return hasPathSum(root.left, val) || hasPathSum(root.right, val);
+        }
+    }
+
 }
 
 
