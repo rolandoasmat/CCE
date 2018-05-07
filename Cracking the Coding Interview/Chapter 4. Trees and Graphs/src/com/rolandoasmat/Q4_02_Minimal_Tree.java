5f92603402aa7f684487e6857675e762c353024e
Rolando Asmat <rolandoasmat@gmail.com>
2018-05-07 12:12:34 -0500

    minimal bst tree

---
+++ Cracking the Coding Interview/Chapter 4. Trees and Graphs/src/com/rolandoasmat/Q4_02_Minimal_Tree.java
@@ -0,0 +1,31 @@
+package com.rolandoasmat;
+
+public class Q4_02_Minimal_Tree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+    }
+
+    /**
+     * Transform a sorted array with unique numns into a min height BST
+     * @param a Sorted Array
+     * @return MST of min size
+     */
+    public Node transform(int[] a) {
+        return transform(a, 0, a.length-1);
+    }
+    private Node transform(int[] a, int i, int j) {
+        if (i <= j) {
+            int mid = (i + j)/2;
+            Node n = new Node();
+            n.data = a[mid];
+            n.left = transform(a, i, mid-1);
+            n.right = transform(a, mid+1, j);
+            return n;
+        } else {
+            return null;
+        }
+    }
+}
